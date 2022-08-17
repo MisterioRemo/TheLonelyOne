646551b6102970785b9326246b7bcf4a9df7208b
MisterioRemo <misterioremo@gmail.com>
2022-08-18 01:24:59 +0300

    SpeakableObject

---
+++ Assets/Scripts/Objects/SpeakableObject.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace TheLonelyOne
+{
+  public class SpeakableObject : InteractableObject
+  {
+    #region COMPONENTS
+    [SerializeField] protected DialogueParticipant dialogueParticipant;
+    #endregion
+
+    #region InteractableObject
+    public override void Interact()
+    {
+      base.Interact();
+      dialogueParticipant.Interact();
+    }
+    #endregion
+  }
+}
