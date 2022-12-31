8ee6c769b5a270de01af352afba92002f93232fc
MisterioRemo <misterioremo@gmail.com>
2022-12-31 19:19:03 +0300

    Добавлен класс SerializableHashSet

---
+++ Assets/Scripts/DataPersistence/SerializableTypes/SerializableHashSet.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TheLonelyOne.SerializableTypes
+{
+  [System.Serializable]
+  public class SerializableHashSet<T> : HashSet<T>, ISerializationCallbackReceiver
+  {
+    public List<T> values = new List<T>();
+    public void OnAfterDeserialize()
+    {
+      Clear();
+      foreach (T value in values)
+        Add(value);
+    }
+
+    public void OnBeforeSerialize()
+    {
+      values.Clear();
+
+      foreach (T value in this)
+        values.Add(value);
+    }
+  }
+}
